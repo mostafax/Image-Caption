432a69c3c47f1d00f8bd0db579bd18d24e270fc2
Raamyy <ramyeg26@gmail.com>
2018-10-24 23:50:38 +0200

    Added text preparation (preproccessing)

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,1 +1,15 @@
-#Text Prepration Should be done Here
+f = open("Flickr8k.token.txt",'r')
+w = open("train.txt",'w')
+fullText=f.read().split("\n")
+for i in range(30000): #first 30000 data are for training
+    fullImage = fullText[i]
+    imageName=fullImage.split("#")[0]
+    imageCaption=fullImage.split("	")[1]
+    w.write(imageName)
+    w.write("\t")
+    w.write("<start>")
+    w.write(imageCaption)
+    w.write("<end>")
+    w.write("\n")
+f.close()
+w.close()
