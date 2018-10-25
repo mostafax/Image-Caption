e61dfb6e3fadf14a056026d62bc7809d1eb32fbf
Raamyy <ramyeg26@gmail.com>
2018-10-25 20:32:12 +0200

    Modefied pre-processing file

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,15 +1,23 @@
-f = open("Flickr8k.token.txt",'r')
-w = open("train.txt",'w')
-fullText=f.read().split("\n")
-for i in range(30000): #first 30000 data are for training
-    fullImage = fullText[i]
-    imageName=fullImage.split("#")[0]
-    imageCaption=fullImage.split("	")[1]
-    w.write(imageName)
-    w.write("\t")
-    w.write("<start>")
-    w.write(imageCaption)
-    w.write("<end>")
-    w.write("\n")
-f.close()
-w.close()
+def createfiles():  # create output files with the desired format of data
+    tokenFile = open("Flickr8k.token.txt",'r')
+    trainOutputFile = open("train.txt",'w')
+    devOutputFile = open("dev.txt",'w')
+    testOutputFile = open("test.txt",'w')
+    fullText=tokenFile.read().split("\n")  # list of all raw data
+    for i in range(40000):  # data contains 8000 photo each has 5 captions
+        fullImage = fullText[i]
+        imageName=fullImage.split("#")[0]
+        imageCaption=fullImage.split("	")[1]
+        if i < 30000:  # first 6000*5 image for train
+            trainOutputFile.write(imageName +"\t<start>"+imageCaption+"<end>\n")
+        elif i<35000: # next 1000*5 image for dev
+            devOutputFile.write(imageName +"\t<start>"+imageCaption+"<end>\n")
+        else:  # next 1000*5 image for test
+            testOutputFile.write(imageName + "\t<start>" + imageCaption + "<end>\n")
+    tokenFile.close()
+    trainOutputFile.close()
+    devOutputFile.close()
+    testOutputFile.close()
+    return
+
+createfiles()
